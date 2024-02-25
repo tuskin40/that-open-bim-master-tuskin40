583d40eafed3783141d5f358bc2010dd240e1be6
uNick Lovelace <tuskin40@users.noreply.github.com>
2024-02-25 20:51:16 +0100

    end of lesson 3.6

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,12 @@
-import { Project, IProject, ProjectStatus, UserRole } from "./classes/Project"
-
+import { IProject, ProjectStatus, UserRole } from "./classes/Project"
+import { ProjectsManager } from "./classes/ProjectsManager"
 
 // const showModal = () => {
 //     const modal = document.getElementById("new-project-modal")
 //     modal.showModal()
 // }
 
+// functions for modal form display
 function showModal(id: string){
     const modal = document.getElementById(id)
     if (modal && modal instanceof HTMLDialogElement) {
@@ -15,14 +16,50 @@ function showModal(id: string){
     }
 }
 
-const newProjectBtn = document.getElementById("new-project-btn")
+function closeModal(id: string){
+    const modal = document.getElementById(id)
+    if (modal && modal instanceof HTMLDialogElement) {
+        modal.close()
+    } else {
+        console.warn("Element:" + id + " was not found")
+    }
+}
 
+function toggleModal(id: string){
+    const modal = document.getElementById(id)
+    if (modal && modal instanceof HTMLDialogElement) {
+        if (modal.open) {
+            modal.close()
+        } else {
+            modal.showModal()
+        }
+    } else {
+        console.warn("Element:" + id + " was not found")
+    }
+}
+
+
+// New Project button code
+const newProjectBtn = document.getElementById("new-project-btn")
 if (newProjectBtn) {
-    newProjectBtn.addEventListener("click", () => {showModal("new-project-modal")})
+    newProjectBtn.addEventListener("click", () => {toggleModal("new-project-modal")})
 } else {
     console.warn("New project button was not found")
 }
 
+// Cancel New project button
+const cancelFormBtn = document.getElementById("cancel-new-project-button")
+if (cancelFormBtn) {
+    cancelFormBtn.addEventListener("click", () => {toggleModal("new-project-modal")})
+} else {
+    console.warn("New cancel-new-project-button was not found")
+}
+
+// Project Manager code
+const projectListUI = document.getElementById("projects-list") as HTMLElement
+const projectsManager = new ProjectsManager(projectListUI)
+
+// Read form data and create project card code
 const projectForm = document.getElementById("new-project-form")
 
 if (projectForm && projectForm instanceof HTMLFormElement)
@@ -37,10 +74,14 @@ if (projectForm && projectForm instanceof HTMLFormElement)
             "status": formData.get("status") as ProjectStatus,
             "finishDate": new Date(formData.get("finishDate") as string),
         }
-        const myProject = new Project(projectObj)
+        const myProject = projectsManager.newProject(projectObj)
+        projectForm.reset()
+        toggleModal("new-project-modal")
         console.log("Project data:", myProject)
     })
+
 } else {
     console.warn("The project from was not found. check the ID!")
    
 }
+
